5a8030d7936adf8c43a010307cc5071bef4d16a5
jackmpcollins <jackmpcollins@gmail.com>
2018-02-25 18:30:47 +0000

    last two parts of Q4 started - probably not working

--- assignment2_advanced/src/overfit_fcnet.py
+++ assignment2_advanced/src/overfit_fcnet.py
@@ -10,7 +10,21 @@ TODO: Overfit the network with 50 samples of CIFAR-10
 ###########################################################################
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
+# Parameters
+hidden_dims = 2
+update_rule = 'sgd' # ['sgd', 'sgd_momentum']
+num_epochs = 20
 
+# Get data and limit to 50 samples (?)
+data = get_CIFAR10_data()
+
+# Create net and train
+net = FullyConnectedNet(hidden_dims, input_dim=32*32*3, num_classes=10, dropout=0)
+slvr = Solver(model, data, update_rule=update_rule, num_epochs=num_epochs)
+slvr.train()
+
+# Check accuracy
+slvr.check_accuracy(data['X_test'], data['y_test'])
 
 ##############################################################################
 #                             END OF YOUR CODE                               #
--- assignment2_advanced/src/train_fcnet.py
+++ assignment2_advanced/src/train_fcnet.py
@@ -11,6 +11,21 @@ accuracy on the validation set.
 ###########################################################################
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
+# Parameters
+hidden_dims = 2
+update_rule = 'sgd' # ['sgd', 'sgd_momentum']
+num_epochs = 20
+
+# Get data
+data = get_CIFAR10_data()
+
+# Create net and train
+net = FullyConnectedNet(hidden_dims, input_dim=32*32*3, num_classes=10, dropout=0)
+slvr = Solver(model, data, update_rule=update_rule, num_epochs=num_epochs)
+slvr.train()
+
+# Check accuracy
+slvr.check_accuracy(data['X_test'], data['y_test'])
 
 ##############################################################################
 #                             END OF YOUR CODE                               #

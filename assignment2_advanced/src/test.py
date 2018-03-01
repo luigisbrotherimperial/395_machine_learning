bcc4b5de11c2608534a6bf9cbaf697fd76605255
Jack  Collins <jack.collins17@imperial.ac.uk>
2018-03-01 17:58:39 +0000

    template for Q5

---
+++ assignment2_advanced/src/test.py
@@ -0,0 +1,16 @@
+def test_fer_model(img_folder, model="/path/to/model"):
+    """
+    Given a folder with images, load the images and your best model to predict the facial expressions of each image.
+    Args:
+    - img_folder: Path to the images to be tested
+    Returns:
+    - preds: A numpy vector of size N with N being the number of images in img_folder.
+    """
+    preds = None
+
+    # load images
+    # load model
+    # predict expressions
+
+    return preds
+    

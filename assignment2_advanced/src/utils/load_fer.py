10cd40abafbfecb21609f9a1eb1e7ff4db6b5074
Jack  Collins <jack.collins17@imperial.ac.uk>
2018-03-06 14:14:25 +0000

    fixed loading fer data

--- assignment2_advanced/src/utils/load_fer.py
+++ assignment2_advanced/src/utils/load_fer.py
@@ -3,39 +3,36 @@ import pandas as pd
 import numpy as np
 from keras.preprocessing.image import img_to_array, load_img
 
-def load_fer_data(img_folder, num_train=28708, num_test=3588):
-    path_train = img_folder + "/Train/*.jpg"
-    path_test = img_folder + "/Test/*.jpg"
-    path_labels = img_folder + "/labels_public.txt"
+def load_fer_data(fer_folder, num_train=28708, num_test=3588):
+    if not fer_folder.endswith('/'):
+        fer_folder += '/'
 
-    train_filelist = glob.glob(path_train)[:num_train]
-    test_filelist = glob.glob(path_test)[:num_test]
-    labels = pd.read_table(path_labels, delimiter=",")
+    path_labels = fer_folder + "/labels_public.txt"
+    labels = pd.read_table(path_labels, delimiter=",").sort_values('img')
+    train_df = labels[labels['img'].apply(lambda x: x[:5] == 'Train')].reset_index()
+    test_df = labels[labels['img'].apply(lambda x: x[:4] == 'Test')].reset_index()
 
-    print('Loading X_train...')
-    #X_train = np.array([np.array(Image.open(fname)) for fname in train_filelist])
+    print('Loading X_train and y_train...')
     X_train = np.ndarray(shape=(num_train, 48, 48, 3), dtype=np.float32)
+    y_train = np.zeros(num_train, int)
     for i in range(num_train):
         if i % 100 == 0:
             print(i, 'of', num_train, 'training images loaded')
-        X_train[i] = img_to_array(load_img(train_filelist[i]))
+        X_train[i] = img_to_array(load_img(fer_folder + train_df['img'][i]))
+        y_train[i] = train_df['emotion'][i]
 
-    print('Loading y_train...')
-    y_train = np.array(labels[labels['img'].str.match('Train')]["emotion"][:num_train])
-
-    print('Loading X_test...')
-    #X_test = np.array([np.array(Image.open(fname)) for fname in test_filelist])
+    print('Loading X_test and y_test...')
     X_test = np.ndarray(shape=(num_test, 48, 48, 3), dtype=np.float32)
+    y_test = np.zeros(num_test, int)
     for i in range(num_test):
         if i % 100 == 0:
             print(i, 'of', num_test, 'testing images loaded')
-        X_test[i] = img_to_array(load_img(test_filelist[i]))
-
-    print('Loading y_test...')
-    y_test = np.array(labels[labels['img'].str.match('Test')]["emotion"][:num_test])
+        X_test[i] = img_to_array(load_img(fer_folder + test_df['img'][i]))
+        y_test[i] = test_df["emotion"][i]
 
     print("X_train shape: " + str(X_train.shape))
     print("y_train shape: " + str(y_train.shape))
     print("X_test shape:  " + str(X_test.shape))
     print("y_test shape:  " + str(y_test.shape))
+
     return X_train, y_train, X_test, y_test
